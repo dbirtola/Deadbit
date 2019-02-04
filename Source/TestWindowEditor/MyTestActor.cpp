bf35da956210d739bb82fd19f1b23537e96a2750
dbirtola <dbirtola@gmail.com>
2019-02-04 16:15:49 -0700

    Bringing dialogue files back

---
+++ Source/TestWindowEditor/MyTestActor.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MyTestActor.h"
+
+// Sets default values
+AMyTestActor::AMyTestActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyTestActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyTestActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
